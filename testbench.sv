eac1739ff0015d19c83e520faed0378cb03f9a35
Mridhula A <164215166+iammridhula@users.noreply.github.com>
2025-06-16 11:33:42 +0530

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -2,11 +2,13 @@ module tb_top;
   reg clk, reset;
   top uut(.clk(clk), .reset(reset));
   initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb_top);
     clk=0;
     reset=1;
     #5;
     reset=0;
-    #400;
+    #400 $finish;
   end
   always begin
     #5
